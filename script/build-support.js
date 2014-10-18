761010d154adfdd46e7a6a29cebcaa5f547b16ed
wooorm <tituswormer@gmail.com>
2014-10-18 17:48:08 +0200

    Move scripts to `script/`

--- script/build-support.js
+++ script/build-support.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var fs = require('fs'),
-    words = require('./data/buzzwords.json');
+    words = require('../data/buzzwords.json');
 
 fs.writeFileSync('Supported-buzzwords.md',
     'Supported Buzzwords\n' +
