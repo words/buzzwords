977d2f9b63a88baabad06893591dc1e1f7579c4c
Titus Wormer <tituswormer@gmail.com>
2017-01-09 10:30:24 +0100

    Replace `scraperjs` with `jsdom`

--- script/generate.js
+++ script/generate.js
@@ -1,36 +1,17 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module buzzwords:script
- * @fileoverview Generate a dictionary of entity names to replacements.
- */
-
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
-var path = require('path');
-var Scraper = require('scraperjs');
+var bail = require('bail');
+var jsdom = require('jsdom');
 
-/* Read. */
-Scraper.DynamicScraper
-  .create('http://en.wikipedia.org/wiki/List_of_buzzwords')
-  .scrape(scrape, function (results) {
-    var list = JSON.stringify(parse(results), 0, 2) + '\n';
-    fs.writeFileSync(path.join(__dirname, 'index.json'), list);
-  });
+jsdom.env('http://en.wikipedia.org/wiki/List_of_buzzwords', done);
 
-/**
- * Scrape.
- *
- * @return {Array.<string>} - List of buzzwords.
- */
-function scrape() {
-  /* global document */
-  var SELECTOR = '#mw-content-text > ul li, .columns > ul li';
+function done(err, win) {
+  var values;
 
-  return [].slice.call(document.querySelectorAll(SELECTOR))
+  bail(err);
+
+  values = [].slice.call(win.document.querySelectorAll('#mw-content-text > ul li, .columns > ul li'))
     .filter(function (node) {
       return !node.className.trim();
     })
@@ -46,17 +27,7 @@ function scrape() {
     .map(function (node) {
       var head = node.childNodes[0];
       return (head.tagName === 'A' ? head : node).textContent;
-    });
-}
-
-/**
- * Parse a list of buzzword-like values.
- *
- * @param {Array.<string>} values - Parsed values.
- * @return {Array.<string>} - Buzzwords.
- */
-function parse(values) {
-  values = values
+    })
     .map(function (value) {
       var match;
 
@@ -80,16 +51,16 @@ function parse(values) {
     })
     .filter(function (value) {
       var head = value.charAt(0);
-      return head !== '-' && head !== '_';
+      return head !== '-' && head !== '_' && value.charAt(value.length - 1) !== '?';
     })
     .filter(function (value) {
       return value !== 'uc' && value !== 'that' && !/_blank_/.test(value);
     })
-    .filter(Boolean);
-
-  return values
-    .filter(function (value, index) {
-      return values.indexOf(value, index + 1) === -1;
+    .filter(Boolean)
+    .filter(function (value, index, all) {
+      return all.indexOf(value, index + 1) === -1;
     })
     .sort();
+
+  fs.writeFile('index.json', JSON.stringify(values, 0, 2) + '\n', bail);
 }
