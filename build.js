7c4349e3eeadd3acd3f170e3b4003874dcd3e7b7
Titus Wormer <tituswormer@gmail.com>
2021-04-05 14:48:44 +0200

    Update dev-dependencies

--- build.js
+++ build.js
@@ -11,20 +11,18 @@ var toString = require('hast-util-to-string')
 
 https.get('https://en.wikipedia.org/wiki/List_of_buzzwords', onresponse)
 
-function onresponse(res) {
-  res.pipe(concat(onconcat)).on('error', bail)
+function onresponse(response) {
+  response.pipe(concat(onconcat)).on('error', bail)
 }
 
 function onconcat(buf) {
-  var tree = unified()
-    .use(parse)
-    .parse(buf)
+  var tree = unified().use(parse).parse(buf)
 
   var values = selectAll('#mw-content-text > ul li, .columns > ul li', tree)
-    .map(function(node) {
+    .map(function (node) {
       return toString(node.children[0])
     })
-    .map(function(value) {
+    .map(function (value) {
       // Split comment from buzzword.
       var match = value.match(/^([\s\S+]+?)( [-–/] ?|\(|,)/)
 
@@ -40,11 +38,11 @@ function onconcat(buf) {
 
       return value.toLowerCase().trim()
     })
-    .filter(function(value) {
+    .filter(function (value) {
       var head = value.charAt(0)
       return value !== 'uc' && head !== '-' && head !== '_'
     })
-    .filter(function(value, index, all) {
+    .filter(function (value, index, all) {
       return !all.includes(value, index + 1)
     })
     .sort()
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var test = require('tape')
 var buzzwords = require('.')
 
-test('buzzwords', function(t) {
+test('buzzwords', function (t) {
   t.equal(Array.isArray(buzzwords), true, 'should be an array')
   t.notEqual(buzzwords.indexOf('millennial'), -1, 'should contain words')
 
